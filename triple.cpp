0c84ea21f0b64a586f2a108850f27a22bd0fe774
johnma <ma.jonathan02@gmail.com>
2022-04-22 01:03:51 -0400

    added documentation

--- triple.cpp
+++ triple.cpp
@@ -9,29 +9,29 @@ int main(){
     std::cin >> testcases;
     i = 0;
     while(i < testcases)
-        std::cin >> vectlen;
+        std::cin >> vectlen; //Grab vector length
         j = 0;
-        while (j < vectlen)
+        while (j < vectlen) //Grab vector values
             std::cin >> helper;
             vect.push_back(helper);
             j++;
-        std::sort(vect.begin(), vect.end());
+        std::sort(vect.begin(), vect.end()); //Sort vector
         j = 0;
         last = -1;
-        while(j < vectlen)
+        while(j < vectlen) //Iterate through vector
             if(vect[j] == last)
-                counter++;
+                counter++; //Match? increment counter
             else
-                counter = 0;
+                counter = 0; //Else, reset, and set last to curr
                 last = vect[j];
-            if(counter == 3)
+            if(counter == 3) //Three occurrances? add to output vector, and EXIT the loop
                 outputs.push_back(last);
                 j = vectlen + 1;
             j++;
-        if(j == vectlen)
-            outputs.push_back(-1);
+        if(j == vectlen) //This should only be true if no match is found
+            outputs.push_back(-1); 
         i++;
-        vect.clear();
+        vect.clear(); 
     for(int output : outputs)
         cout << output << "\n";
     return 0;
