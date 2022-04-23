555885f04af15ed159da6b585ecf23a5783b4a0e
johnma <ma.jonathan02@gmail.com>
2022-04-23 00:58:33 -0400

    Hello from Delaware

--- oddeven.cpp
+++ oddeven.cpp
@@ -1,35 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 typedef long long ll;
 
 using namespace std;
 
 int main(){
-    int T, N, j, k, last, parity;
+    int T, N, j, k, last, parity, possible;
     vector<std::string> outputs;
     std::cin >> T;
     for(int i = 0; i < T; i++){
         std::cin >> N; //grab vec len
         vector<int> vect(N); 
         for(int &value : vect) std::cin >> value;
-        j = 0; 
-        if(N % 2 == 1){k = N - 1;} //if vec len is odd, subtract one 
-        last = vect[0];
-        while(j < k){        //processing even indices
-            if(last % 2 != vect[j] %2){
-                parity = 0;
-                j = k;
+        vector<int> odds;
+        vector<int> evens;
+        
+        k = 1;
+        while(k < N){
+            odds.push_back(vect[k]);
+            k+=2;
+        }
+        k = 0;
+        while(k < N){
+            evens.push_back(vect[k]);
+            k+=2;
+        }
+
+        parity = odds[0] % 2;
+        for(int value : odds){
+            if(parity == value % 2){
+                possible = 1;
             }
             else{
-                j += 2;
+                outputs.push_back("NO");
+                possible = 0;
+                break;
             }
         }
-        if(j == k+2){parity = 1}
-        if(!parity){
-            outputs.push_back("NO");
-            continue;}
         
-        j = 1;
-        if(N % 2 == 1){k = N}
-        else{k = N - 1}
+        if(!possible){continue;}
+
+        parity = evens[0] % 2;
+        for(int value : evens){
+            if(parity == value % 2){
+                possible = 1;
+            }
+            else{
+                outputs.push_back("NO");
+                possible = 0;
+                break;
+            }
+        }
+        if(possible){outputs.push_back("YES");}
+    }
+    for(std::string output : outputs) std::cout << output << "\n";
+    return 0;
+}
+
+
+        
+
+
+        
+
+        
+
 
 
