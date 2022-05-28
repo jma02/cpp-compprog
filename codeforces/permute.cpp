4dda88af7e930746e0a7509da668f9ca7e81f5be
johnma <ma.jonathan02@gmail.com>
2022-05-28 17:27:50 -0400

    sat. may 28, 2022

---
+++ codeforces/permute.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+
+vector<int> 
+void search(){
+
+}
+
+int main(){
+
+}
--- codeforces/whiteboard.cpp
+++ codeforces/whiteboard.cpp
@@ -6,20 +6,29 @@ using namespace std;
 
 void solve(){
     int n; cin >> n;
-    vector<int> whiteboard;
+    multiset<int> whiteboard;
     for(int i = 1; i <= n; i++){
-        whiteboard.pb(i);
+        whiteboard.insert(i);
     }
+
     vector<pair<int,int>> operations;
     for(int i = 0; i < n-1; i++){
-        operations.pb(mp(whiteboard[1], whiteboard[whiteboard.size()-1]));
-        whiteboard.erase(whiteboard.begin()+1);
-        whiteboard.pop_back();
+        auto iter = whiteboard.end();
+        iter--;
+        int a = *iter;
+        whiteboard.erase(iter);
+        iter = whiteboard.end();
+        iter--;
+        int b = *iter;
+        whiteboard.erase(iter);
+        operations.pb(mp(a,b));
+        whiteboard.insert((a+b+1)/2);
     }
-    cout << whiteboard[0] << '\n';
+    cout << *whiteboard.begin() << '\n';
     for(auto p : operations){
         cout << p.first << " " << p.second << '\n';
     }
+    
 }
 int main(){
     int T; cin >> T;
