c3d3048068d15216a48428bbe2e0814c2199db09
johnma <ma.jonathan02@gmail.com>
2022-06-10 22:21:45 -0400

    update template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(v) v.begin(), v.end()
+
+typedef long long ll
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+
+}
